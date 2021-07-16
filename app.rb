bcfa4f12bb86b885d48c5112e8ef3bfc5e797f62
Codi Kraus <codi.kraus@gmail.com>
2021-07-16 15:56:49 -0500

    commit tree de32a0a0ad668ee38be44e764e5ed803c1b26558

--- app.rb
+++ app.rb
@@ -1,4 +1,14 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+    get '/' do
+        erb :user_input
+    end
+
+    post '/piglatinize' do
+        text_from_user = params[:user_phrase]
+        @analyzed_text = PigLatinizer.new
+        @pig_latin  = @analyzed_text.piglatinize(text_from_user)
+        erb :piglatinize
+    end
 end
